4775ff813d76d2d35b00b51f333b9733a285a4dd
Bujo11 <roybujo12@gmail.com>
2022-12-22 12:45:26 -0800

    lklkl

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,9 @@
-nclude "main.h"
-
+#include "main.h"
 /**
  *  * cap_string - converts string words to be capitalized
  *   * @s: input string to have all words capitalized
  *    * Return: string after conversion to Capitalize words
  *     */
-
 char *cap_string(char *s)
 {
 		int i;
